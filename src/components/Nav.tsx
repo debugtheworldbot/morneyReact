bbc336e2fa15291275067e9ace1f667ddb097153
debugtheworldbot <istiancz@gmail.com>
2020-05-25 15:58:41 +0800

    add 3 icons of nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+require("icons/money.svg");
+require("icons/tag.svg");
+require("icons/chart.svg")
+
+
 
 const NavWrapper=styled.nav`
 line-height: 24px;
@@ -10,7 +15,16 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
    >li{
    width: 33.333333333%;
    text-align: center;
-   padding: 16px;
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+    .icon{
+      width: 24px;
+      height: 24px;
+      margin: 5px 0;
+    }
+    
    }
  }
 `
@@ -20,13 +34,21 @@ const Nav=()=>{
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className={"icon"}>
+                        <use xlinkHref="#tag"/>
+                    </svg>
                     <Link to="/tags">标签页</Link>
-
                 </li>
                 <li>
+                    <svg className={"icon"}>
+                        <use xlinkHref="#money"/>
+                    </svg>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
+                    <svg className={"icon"}>
+                        <use xlinkHref="#chart"/>
+                    </svg>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
