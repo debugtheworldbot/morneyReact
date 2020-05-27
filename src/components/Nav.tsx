ac775f8cf7809c1276d378fe6987ea08b261f541
debugtheworldbot <istiancz@gmail.com>
2020-05-27 09:18:44 +0800

    UI of NoteSection in Money.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,6 +7,7 @@ import Icon from "./icon";
 const NavWrapper = styled.nav`
 line-height: 24px;
 box-shadow: 0 0 3px rgba(0,0,0,0.25);
+background: #ffffff;
  >ul{
     display: flex;
    >li{
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -23,7 +23,22 @@ const TagsSection=styled.section`background:#FFFFFF;padding: 12px 16px;
   }
 `
 const NotesSection=styled.section`
-
+  background:#f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  >label{
+  display: flex;
+  align-items: center;
+   >span{
+    margin-right: 16px;
+   }
+   >input{
+    border: none;
+    background:none;
+    flex-grow: 1;
+    height: 72px;
+   }
+  }
 `
 const CategorySection=styled.section`
 
