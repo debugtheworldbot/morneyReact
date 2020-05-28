4a1bb1cfc3e4526f72aa5b37b4933e8b6273195f
debugtheworldbot <istiancz@gmail.com>
2020-05-28 22:04:57 +0800

    function of NumberPadSection (wip)

--- src/views/Money/NumberpadSection.tsx
+++ src/views/Money/NumberpadSection.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const NumberPadSection=styled.section`
+
+const Wrapper = styled.section`
 display: flex;
 flex-direction: column;
   > .output{
@@ -50,4 +52,53 @@ flex-direction: column;
     }
   }
 `
+const NumberPadSection:React.FC = () => {
+    const [output,setOutput]=useState<string>('0')
+    const buttonWrapper=(e:React.MouseEvent)=>{
+        const text=(e.target as HTMLButtonElement).textContent
+        if(text===null){return}
+        switch (text) {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '0':
+                if(output==='0'){
+                    setOutput(text)
+                }else {
+                    setOutput(output + text)
+                }
+                break;
+        }
+    }
+    return (
+        <Wrapper>
+            <div className={"output"}>
+                {output}
+            </div>
+            <div className={"pad clearfix"} onClick={buttonWrapper}>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button className={"ok"}>OK</button>
+                <button className="zero">0</button>
+                <button>.</button>
+
+            </div>
+        </Wrapper>
+    )
+}
 export default NumberPadSection
