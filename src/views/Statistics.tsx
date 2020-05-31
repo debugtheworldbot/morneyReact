684648ef13ffe6d8b0e75c2a652fee69c8d98264
debugtheworldbot <istiancz@gmail.com>
2020-05-31 22:12:58 +0800

    show all data records in page Statistics.tsx

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -8,6 +8,19 @@ import day from 'dayjs'
 const  CategoryWrapper=styled.div`
   background:white;
 `
+const Item=styled.div`
+  display: flex;
+  justify-content: space-between;
+  background:white;
+  font-size: 18px;
+  line-height: 20px;
+  padding: 10px 16px;
+  >.note{
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+  }
+`
 function Statistics() {
     const [category,setCategory]=useState<'-'|'+'>("-")
     const {records}=useRecords()
@@ -19,13 +32,19 @@ function Statistics() {
             </CategoryWrapper>
             <div>
                 {records.map(r=>{
-                    return <div>
-                        {r.tagIds.map(tagId=>getName(tagId))}
-                        <hr/>
-                        {r.amount}
-                        <hr/>
-                        {day(r.createdAt).format("YYYY年MM月DD日")}
-                    </div>
+                    return <Item>
+                        <div className="tags">
+                            {r.tagIds.map(tagId=>getName(tagId))}
+                        </div>
+                        {r.note && <div className={"note"}>
+                            {r.note}
+                        </div>}
+                        <div className="amount">
+                            ￥{r.amount}
+                        </div>
+
+                        {/*{day(r.createdAt).format("YYYY年MM月DD日")}*/}
+                    </Item>
 
                 })}
             </div>
